9e864d474fe0b20f88c581b84bc8d0dba9cdf4a5
luffy9847 <95870943+pengdejiang@users.noreply.github.com>
2024-06-17 16:05:06 +0800

    Update #github-DEBUG.py

--- ZUZU-PY/#github-DEBUG.py
+++ ZUZU-PY/#github-DEBUG.py
@@ -226,17 +226,15 @@ keywords = ['CCTV','CETV', 'CF', 'IPT淘', 'CHC', 'IWA', '凤凰卫视', '卫视
 
 pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
 
-if re.search(pattern,line) and line.count(',') == 1: #设置条件为一行只有一个逗号的，排除掉一行中有两个以上逗号的行
-
 #pattern = r"^(.*?),(?!#genre#)(.*?)$" #以分类直接复制
 
-     with open('排序.txt', 'r', encoding='utf-8') as file, open('T1.txt', 'w', encoding='utf-8') as T1:    #####定义临时文件名
+with open('排序.txt', 'r', encoding='utf-8') as file, open('T1.txt', 'w', encoding='utf-8') as T1:    #####定义临时文件名
 
-       for line in file:
+    for line in file:
 
-          if re.search(pattern, line):  # 如果行中有任意关键字
+        if re.search(pattern, line):  # 如果行中有任意关键字
 
-            T1.write(line)  # 将该行写入输出文件 #####定义临时文件
+         T1.write(line)  # 将该行写入输出文件 #####定义临时文件
 
 for line in fileinput.input("T1.txt", inplace=True):  #打开文件，并对其进行关键词原地替换    
 
