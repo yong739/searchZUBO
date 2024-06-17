2ec14e2c3c3bd7b3681584d5f97d02fb1ac06f56
luffy9847 <95870943+pengdejiang@users.noreply.github.com>
2024-06-17 11:44:09 +0800

    Update #github-DEBUG.py

--- ZUZU-PY/#github-DEBUG.py
+++ ZUZU-PY/#github-DEBUG.py
@@ -232,7 +232,7 @@ with open('排序.txt', 'r', encoding='utf-8') as file, open('T1.txt', 'w', enco
 
     for line in file:
         if re.search(pattern,line) and line.count(',') == 1: #排除掉一行中有两个以上逗号的行
-        if re.search(pattern, line):  # 如果行中有任意关键字
+        else re.search(pattern, line):  # 如果行中有任意关键字
 
          T1.write(line)  # 将该行写入输出文件 #####定义临时文件
 
