be80b7030d55b41026bcfb04fca0adbb8c62dc55
luffy9847 <95870943+pengdejiang@users.noreply.github.com>
2024-06-17 14:14:11 +0800

    Update #github-DEBUG.py

--- ZUZU-PY/#github-DEBUG.py
+++ ZUZU-PY/#github-DEBUG.py
@@ -226,13 +226,15 @@ keywords = ['CCTV','CETV', 'CF', 'IPT淘', 'CHC', 'IWA', '凤凰卫视', '卫视
 
 pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
 
+if re.search(pattern,line) and line.count(',') == 1: #排除掉一行中有两个以上逗号的行
+    
 #pattern = r"^(.*?),(?!#genre#)(.*?)$" #以分类直接复制
 
 with open('排序.txt', 'r', encoding='utf-8') as file, open('T1.txt', 'w', encoding='utf-8') as T1:    #####定义临时文件名
 
     for line in file:
-        if re.search(pattern,line) and line.count(',') == 1: #排除掉一行中有两个以上逗号的行
-        else re.search(pattern, line):  # 如果行中有任意关键字
+       
+        if re.search(pattern, line):  # 如果行中有任意关键字
 
          T1.write(line)  # 将该行写入输出文件 #####定义临时文件
 
@@ -257,7 +259,7 @@ def custom_sort_key(item):
 
     channel_letters = ''.join(filter(str.isalpha, channel))
     channel_numbers = ''.join(filter(str.isdigit, channel))
-
+    
     if channel_numbers.isdigit():
         channel_sort_key = (channel_letters, int(channel_numbers))
     else:
