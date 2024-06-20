b16445f4ee7615c9a79075f296248bc34004ef75
luffy9847 <95870943+pengdejiang@users.noreply.github.com>
2024-06-20 15:21:43 +0800

    Add files via upload

---
+++ ZUZU-PY/test-one-dou-hao.py
@@ -0,0 +1,299 @@
+﻿import time
+
+import concurrent.futures
+
+from selenium import webdriver
+
+from selenium.webdriver.chrome.options import Options
+
+import requests
+
+import re
+
+import os
+
+import threading
+
+from queue import Queue
+
+from datetime import datetime
+
+import replace
+
+import fileinput
+
+# 合并自定义频道文件#################################################################################################
+
+file_contents = []
+
+file_paths = ["天津联通.txt", "山西联通.txt","安徽电信.txt", "河南联通.txt", "河南电信.txt", "四川电信.txt", "重庆联通.txt", "重庆电信.txt","山东电信.txt","广东电信.txt","广西电信.txt","江西电信.txt","河北电信.txt","浙江电信.txt","湖南电信.txt","辽宁联通.txt","陕西电信.txt","JIEXI.txt"]  # 替换为实际的文件路径列表
+
+for file_path in file_paths:
+
+    with open(file_path, 'r', encoding="utf-8") as file:
+
+        content = file.read()
+
+        file_contents.append(content)
+
+
+# 写入合并后的文件
+
+with open("合并.txt", "w", encoding="utf-8") as output:
+
+    output.write('\n'.join(file_contents))
+
+
+#替换多余的关键字词###################################################################################################
+
+for line in fileinput.input("合并.txt", inplace=True):  #打开文件，并对其进行原地替换
+
+    line = line.replace("CCTV10", "CCTW10")
+
+    line = line.replace("CCTV11", "CCTW11")
+
+    line = line.replace("CCTV12", "CCTW12")
+
+    line = line.replace("CCTV13", "CCTW13")
+
+    line = line.replace("CCTV14", "CCTW14")
+
+    line = line.replace("CCTV15", "CCTW15")
+
+    line = line.replace("CCTV16", "CCTW16")
+
+    line = line.replace("CCTV17", "CCTW17")
+
+    #需要排在前面的频道
+
+    line = line.replace("CCTV1综合", "CCTV1")
+
+    line = line.replace("CCTV2财经", "CCTV2")
+
+    line = line.replace("CCTV3综艺", "CCTV3")
+
+    line = line.replace("CCTV4国际", "CCTV4")
+
+    line = line.replace("CCTV4中文国际", "CCTV4")
+
+    line = line.replace("CCTV4欧洲", "CCTV4")
+
+    line = line.replace("CCTV5体育", "CCTV5")
+
+    line = line.replace("CCTV5+体育", "CCTV5+")
+
+    line = line.replace("CCTV6电影", "CCTV6")
+
+    line = line.replace("CCTV7军事", "CCTV7")
+
+    line = line.replace("CCTV7军农", "CCTV7")
+
+    line = line.replace("CCTV7农业", "CCTV7")
+
+    line = line.replace("CCTV7国防军事", "CCTV7")
+
+    line = line.replace("CCTV8电视剧", "CCTV8")
+
+    line = line.replace("CCTV8纪录", "CCTV9")
+
+    line = line.replace("CCTV9记录", "CCTV9")
+
+    line = line.replace("CCTV9纪录", "CCTV9")
+
+    line = line.replace("CCTV10科教", "CCTV10")
+
+    line = line.replace("CCTV11戏曲", "CCTV11")
+
+    line = line.replace("CCTV12社会与法", "CCTV12")
+
+    line = line.replace("CCTV13新闻", "CCTV13")
+
+    line = line.replace("CCTV新闻", "CCTV13")
+
+    line = line.replace("CCTV14少儿", "CCTV14")
+
+    line = line.replace("央视14少儿", "CCTV14")
+
+    line = line.replace("CCTV少儿超", "CCTV14")
+
+    line = line.replace("CCTV15音乐", "CCTV15")
+
+    line = line.replace("CCTV音乐", "CCTV15")
+
+    line = line.replace("CCTV16奥林匹克", "CCTV16")
+
+    line = line.replace("CCTV17农业农村", "CCTV17")
+
+    line = line.replace("CCTV17军农", "CCTV17")
+
+    line = line.replace("CCTV17农业", "CCTV17")
+
+    line = line.replace("CCTV5+体育赛视", "CCTV5+")
+
+    line = line.replace("CCTV5+赛视", "CCTV5+")
+
+    line = line.replace("CCTV5+体育赛事", "CCTV5+")
+
+    line = line.replace("CCTV5+赛事", "CCTV5+")
+
+    line = line.replace("CCTV5+体育", "CCTV5+")
+
+    line = line.replace("CCTV5赛事", "CCTV5+")
+
+
+
+    print(line, end="")  #设置end=""，避免输出多余的换行符
+
+
+
+#二次替换某些关键词为便于排序的自定义词####################################################################################################
+
+for line in fileinput.input("合并.txt", inplace=True):  #打开文件，并对其进行原地替换
+
+    
+    line = line.replace("CCTV10", "CCTW10")
+
+    line = line.replace("CCTV11", "CCTW11")
+
+    line = line.replace("CCTV12", "CCTW12")
+
+    line = line.replace("CCTV13", "CCTW13")
+
+    line = line.replace("CCTV14", "CCTW14")
+
+    line = line.replace("CCTV15", "CCTW15")
+
+    line = line.replace("CCTV16", "CCTW16")
+
+    line = line.replace("CCTV17", "CCTW17")
+
+ 
+
+    print(line, end="")  #设置end=""，避免输出多余的换行符
+
+
+
+#对替换完成的文本进行排序#####################################################################################################################
+
+
+
+with open('合并.txt', 'r', encoding='utf-8') as f:
+
+    lines = f.readlines()
+
+
+lines.sort()
+
+
+with open('排序.txt', 'w', encoding='UTF-8') as f:
+
+    for line in lines:
+
+        f.write(line)
+
+
+#再次替换自定义词为常规词##########################################################################################################################
+
+for line in fileinput.input("排序.txt", inplace=True):  #打开文件，并对其进行原地替换
+
+    line = line.replace("CCTW10", "CCTV10")
+
+    line = line.replace("CCTW11", "CCTV11")
+
+    line = line.replace("CCTW12", "CCTV12")
+
+    line = line.replace("CCTW13", "CCTV13")
+
+    line = line.replace("CCTW14", "CCTV14")
+
+    line = line.replace("CCTW15", "CCTV15")
+
+    line = line.replace("CCTW16", "CCTV16")
+
+    line = line.replace("CCTW17", "CCTV17")
+
+
+
+    print(line, end="")  #设置end=""，避免输出多余的换行符
+
+ ##################################################################################################################################SPLIT#
+
+#开始#########################
+#从整理好的文本中按类别进行特定关键词提取#############################################################################################
+
+keywords = ['CCTV','CETV', 'CF', 'IPT淘', 'CHC', 'IWA', '凤凰卫视', '卫视', '金鹰卡通', '纪实科教', '卡酷少儿', '嘉佳卡通', '哈哈炫动', '乐游频道', '动漫秀场', '新动漫','纪实人文', '金色学堂',  '纪实科教', '金鹰纪实', '求索记录']  # 需要提取的关键字列表
+
+pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
+
+#pattern = r"^(.*?),(?!#genre#)(.*?)$" #以分类直接复制
+
+if re.search(pattern,line) and line.count(',') == 1:
+
+    with open('排序.txt', 'r', encoding='utf-8') as file, open('T1.txt', 'w', encoding='utf-8') as T1:    #####定义临时文件名
+
+      for line in file:
+
+        if re.search(pattern, line) and line.count(',') == 1:  # 如果行中有任意关键字而且行内只有一个逗号
+
+         T1.write(line)  # 将该行写入输出文件 #####定义临时文件
+
+for line in fileinput.input("T1.txt", inplace=True):  #打开文件，并对其进行关键词原地替换    
+
+    print(line, end="")  #设置end=""，避免输出多余的换行符          
+
+#新建待合并临时TTxxx.TXT文件并在抬头写入频道编码genre###################
+with open('TT1.txt', 'w', encoding='utf-8') as TT1:    #####定义临时文件名
+
+    TT1.write('\n📺中国卫视秧视,#genre#\n')        
+ 
+    print(line, end="")  #设置end=""，避免输出多余的换行符 
+#写入完成-进入下一步排序######################
+
+#对相同频道IP排序--域名在前###################
+import re
+
+# A版本--自定义排序键函数 固定域名--在前
+def custom_sort_key(item):
+    channel, url = item.split(',')
+
+    channel_letters = ''.join(filter(str.isalpha, channel))
+    channel_numbers = ''.join(filter(str.isdigit, channel))
+
+    if channel_numbers.isdigit():
+        channel_sort_key = (channel_letters, int(channel_numbers))
+    else:
+        channel_sort_key = (channel_letters, 0)
+
+    sort_key = re.search(r"http://(.*?)\.", url)
+    if sort_key:
+        sort_key = sort_key.group(1)
+    else:
+        sort_key = url
+
+    # 检查sort_key是否为数字
+    if sort_key[0].isalpha():
+        sort_key = (0, sort_key)  # 字母开头的sort_key排在最前面
+    elif sort_key.isdigit():
+        sort_key = (1, -int(sort_key))  # 数字从大到小排序
+    else:
+        sort_key = (2, sort_key)
+
+    return (channel_sort_key, sort_key)
+
+with open('T1.txt', 'r', encoding="utf-8") as input_file, open('TT1.txt', 'a', encoding="utf-8") as output_file:
+    # 读取所有行并存储在列表中
+    lines = input_file.readlines()
+
+    # 过滤掉空白行
+    lines = [line.strip() for line in lines if line.strip()]
+    
+    sorted_data = sorted(lines, key=custom_sort_key)
+
+    # 将排序后的数据写入输出文件
+    for channels in sorted_data: 
+        output_file.write(f"{channels}\n")
+    sorted_data = sorted(lines, key=custom_sort_key)
+
+   #结束########################################################
+
+ ##################################################################################################################################SPLIT#
