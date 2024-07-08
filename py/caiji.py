d3355d64e00825623b7082d2952a8570a7cd5114
yong-del <uxjagle401@mailpod.site>
2024-07-08 09:20:55 +0800

    Update caiji.py

--- py/caiji.py
+++ py/caiji.py
@@ -137,36 +137,9 @@ for keyword in keywords:
             else:
                 print(f"{current_time} 搜索IPTV频道源[]，超时次数过多：{timeout_cnt} 次，停止处理")
 
-# 获取outfiles目录下的文件名
-    # files1 = os.listdir('outfiles')
-    files1 = 'outfiles'
-    # 过滤TXT文件
-    file_contents = []
-    for file_path in files:
-        with open('outfiles/' + file_path, 'r', encoding="utf-8") as file:
-            content = file.read()
-            file_contents.append(content)
 
-        # 移除文件
-        # os.remove('outfiles/' + file_path)
-
-    # 写入合并后的txt文件
-    with open("IPTV_UDP.txt", "w", encoding="utf-8") as output:
-        output.write('\n\n'.join(file_contents))
-        # 写入更新日期时间
-        # file.write(f"{now_today}更新,#genre#\n")
-        # 获取当前时间
-        local_tz = pytz.timezone("Asia/Shanghai")
-        now = datetime.now(local_tz)
-        # now = datetime.now()
-        output.write(f"\n更新时间,#genre#\n")
-        output.write(f"{now.strftime("%Y-%m-%d")},url\n")
-        output.write(f"{now.strftime("%H:%M:%S")},url\n")
-
-    output.close()
-
-    print(f"电视频道成功写入IPTV_UDP.txt")
-#print('节目表制作完成！ 文件输出在当前文件夹！')
+    
+print('节目表制作完成！ 文件输出在当前文件夹！')
 
 
 
