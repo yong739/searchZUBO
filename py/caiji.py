bb9ebdef967ea4008177a51cfaa73231e6c1be5d
yong-del <uxjagle401@mailpod.site>
2024-07-08 11:24:16 +0800

    Update caiji.py

--- py/caiji.py
+++ py/caiji.py
@@ -137,3 +137,22 @@ for keyword in keywords:
             else:
                 print(f"{current_time} 搜索IPTV频道源[]，超时次数过多：{timeout_cnt} 次，停止处理")
 print('节目表制作完成！ 文件输出在当前文件夹！')
+
+# 将当前目录下的所有txt文件合并到一个文件
+
+# key 获取当前目录下的文件列表
+file_list = os.listdir(outfiles)
+
+# 创建一个新文件，用于保存合并后的内容
+with open('result.txt', 'w') as f_result:
+    # 遍历文件列表
+    for filename in file_list:
+        # 只处理txt文件
+        if filename.endswith('.txt'):
+            # 打开txt文件，读取内容
+            with open(filename, 'r',encoding='UTF-8') as f_txt:
+                content = f_txt.read()
+                # 将读取的内容写入到result.txt文件中
+                f_result.write(content)
+                # 写入换行符
+                f_result.write('\n')
