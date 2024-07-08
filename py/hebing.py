edf462060ddcc7bde1211a73e9006c6db94a7283
yong-del <uxjagle401@mailpod.site>
2024-07-08 15:52:32 +0800

    Create hebing.py

---
+++ py/hebing.py
@@ -0,0 +1,18 @@
+#coding=utf-8 
+import os
+import os.path #文件夹遍历函数  
+#获取目标文件夹的路径
+filedir = '/outfiles'
+#获取当前文件夹中的文件名称列表  
+filenames=os.listdir(filedir)
+#打开当前目录下的result.txt文件，如果没有则创建
+f=open('result2.txt','w')
+#先遍历文件名
+for filename in filenames:
+    filepath = filedir+'/'+filename
+    #遍历单个文件，读取行数
+    for line in open(filepath):
+        f.writelines(line)
+    f.write('\n')
+#关闭文件
+f.close()
