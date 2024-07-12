1e628d4958c88fbbe3e310de19f0460552ecc9e4
yong-del <uxjagle401@mailpod.site>
2024-07-12 09:58:39 +0800

    Update caiji.py

--- py/caiji.py
+++ py/caiji.py
@@ -7,45 +7,46 @@ import datetime
 from datetime import datetime
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+import os.path #文件夹遍历函数  
 
-#enter
+# 获取rtp目录下的文件名
 files = os.listdir('rtp')
 
 files_name = []
 
-#enter
+# 去除后缀名并保存至provinces_isps
 for file in files:
     name, extension = os.path.splitext(file)
     files_name.append(name)
 
-#enter
+#忽略不符合要求的文件名
 provinces_isps = [name for name in files_name if name.count('_') == 1]
 
-#enter
-print(f"enter：{provinces_isps}enter") 
+# 打印结果
+print(f"本次查询：{provinces_isps}的组播节目") 
 
 keywords = []
 
 for province_isp in provinces_isps:
-    #enter
+    # 读取文件并删除空白行
     try:
         with open(f'rtp/{province_isp}.txt', 'r', encoding='utf-8') as file:
             lines = file.readlines()
             lines = [line.strip() for line in lines if line.strip()]
-        #enter
+        # 获取第一行中以包含 "rtp://" 的值作为 mcast
         if lines:
             first_line = lines[0]
             if "rtp://" in first_line:
                 mcast = first_line.split("rtp://")[1].split(" ")[0]
                 keywords.append(province_isp + "_" + mcast)
     except FileNotFoundError:
-    #enter
-        print(f"enter '{province_isp}.txt' enter.")
+    # 如果文件不存在，则捕获 FileNotFoundError 异常并打印提示信息
+        print(f"文件 '{province_isp}.txt' 不存在. 跳过此文件.")
 
 for keyword in keywords:
     province, isp, mcast = keyword.split("_")
-    #enter
-    #enter
+    #将省份转成英文小写
+    # 根据不同的 isp 设置不同的 org 值
     if province == "北京" and isp == "联通":
         isp_en = "cucc"
         org = "China Unicom Beijing Province Network"
@@ -69,70 +70,71 @@ for keyword in keywords:
         try:
             search_url = 'https://fofa.info/result?qbase64='
             search_txt = f'\"udpxy\" && country=\"CN\" && region=\"{province}\" && org=\"{org}\"'
-                #enter
+                # 将字符串编码为字节流
             bytes_string = search_txt.encode('utf-8')
-                #enter
+                # 使用 base64 进行编码
             search_txt = base64.b64encode(bytes_string).decode('utf-8')
             search_url += search_txt
-            print(f"{current_time} enter : {province}{isp} ，enter : {search_url}")
+            print(f"{current_time} 查询运营商 : {province}{isp} ，查询网址 : {search_url}")
             response = requests.get(search_url, timeout=5)
-            #enter
+            # 处理响应
             response.raise_for_status()
-            #enter
+            # 检查请求是否成功
             html_content = response.text
-            #enter
+            # 使用BeautifulSoup解析网页内容
             html_soup = BeautifulSoup(html_content, "html.parser")
             # print(f"{current_time} html_content:{html_content}")
-            #enter
-            #enter
+            # 查找所有符合指定格式的网址
+            # 设置匹配的格式，如http://8.8.8.8:8888
             pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"
             urls_all = re.findall(pattern, html_content)
-            #enter
+            # 去重得到唯一的URL列表
             result_urls = set(urls_all)
             print(f"{current_time} result_urls:{result_urls}")
 
             valid_ips = []
 
-            #enter
+            # 遍历所有视频链接
             for url in result_urls:
                 video_url = url + "/rtp/" + mcast
 
-                #enter
+                # 用OpenCV读取视频
                 cap = cv2.VideoCapture(video_url)
 
-                #enter
+                # 检查视频是否成功打开
                 if not cap.isOpened():
-                    print(f"{current_time} {video_url} enter")
+                    print(f"{current_time} {video_url} 无效")
                 else:
-                    #enter
+                    # 读取视频的宽度和高度
                     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
                     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-                    print(f"{current_time} {video_url} enter{width}x{height}")
-                    #enter
+                    print(f"{current_time} {video_url} 的分辨率为 {width}x{height}")
+                    # 检查分辨率是否大于0
                     if width > 0 and height > 0:
                         valid_ips.append(url)
-                    #enter
+                    # 关闭视频流
                     cap.release()
                     
             if valid_ips:
-                #enter
+                #生成节目列表 省份运营商.txt
                 rtp_filename = f'rtp/{province}_{isp}.txt'
                 with open(rtp_filename, 'r', encoding='utf-8') as file:
                     data = file.read()
-                txt_filename = f'{province}{isp}.txt'
+                txt_filename = f'outfiles/{province}{isp}.txt'
                 with open(txt_filename, 'w') as new_file:
+                    new_file.write(f'{province}{isp},#genre#\n')
                     for url in valid_ips:
                         new_data = data.replace("rtp://", f"{url}/rtp/")
                         new_file.write(new_data)
 
-                print(f'enter，enter{txt_filename}')
+                print(f'已生成播放列表，保存至{txt_filename}')
 
         except (requests.Timeout, requests.RequestException) as e:
             timeout_cnt += 1
-            print(f"{current_time} [{province}]enter：{timeout_cnt}")
+            print(f"{current_time} [{province}]搜索请求发生超时，异常次数：{timeout_cnt}")
             if timeout_cnt <= 3:
-                    #enter
+                    # 继续下一次循环迭代
                 continue
             else:
-                print(f"{current_time} enter[]，enter：{timeout_cnt} enter，enter")
-print('enter！')
+                print(f"{current_time} 搜索IPTV频道源[]，超时次数过多：{timeout_cnt} 次，停止处理")
+print('节目表制作完成！ 文件输出在当前文件夹！')
