6187f9bb8df4b7da8db241f5e9e1e8394cf6fd4c
yong-del <uxjagle401@mailpod.site>
2024-07-12 10:05:59 +0800

    Update caiji.py

--- py/caiji.py
+++ py/caiji.py
@@ -120,9 +120,9 @@ for keyword in keywords:
                 rtp_filename = f'rtp/{province}_{isp}.txt'
                 with open(rtp_filename, 'r', encoding='utf-8') as file:
                     data = file.read()
-                txt_filename = f'outfiles/{province}{isp}.txt'
+                txt_filename = f'{province}{isp}.txt'
                 with open(txt_filename, 'w') as new_file:
-                    new_file.write(f'{province}{isp},#genre#\n')
+                   # new_file.write(f'{province}{isp},#genre#\n')
                     for url in valid_ips:
                         new_data = data.replace("rtp://", f"{url}/rtp/")
                         new_file.write(new_data)
