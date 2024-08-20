fd7ae138b24fe98ed7a1c136a89ea28181051e1e
yong739 <yongshou96@gmail.com>
2024-08-20 08:49:33 +0800

    Update 33.py

--- 33.py
+++ 33.py
@@ -13,12 +13,14 @@ eventlet.monkey_patch()
 
 ###urls城市根据自己所处的地理位置修改
 urls = [
-    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjM0NjIiICYmIHJlZ2lvbj0iS2FvaHNpdW5nIiA%3D",#西安
-    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjM0NjIiICYmIHJlZ2lvbj0iVGFpcGVpIg%3D%3D",#宝鸡
-    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjM0NjIiICYmcmVnaW9uPSJUYWljaHVuZyIg",#陕西
-    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBhc249IjM0NjIiICYmIHJlZ2lvbj0iVFci",#安康
- #   "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBvcmc9IkNoaW5hbmV0IiAmJiBjaXR5PSJ3ZWluYW4i",#渭南
- #   "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBvcmc9IkNoaW5hbmV0IiAmJiByZWdpb249IlNoYWFueGkiICYmIGNpdHk9Inl1bGluIg%3D%3D"#榆林
+    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBvcmc9IkNoaW5hbmV0IiAmJiByZWdpb249Imd1YW5nZG9uZyI%3D",  #广东
+    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBvcmc9IkNoaW5hbmV0IiAmJiBjaXR5PSJHdWFuZ3pob3Ui", #广州
+    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBvcmc9IkNoaW5hbmV0IiAmJiBjaXR5PSJzaGVuemhlbiI%3D",  #深圳
+    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBvcmc9IkNoaW5hbmV0IiAmJiBjaXR5PSJodWl6aG91Ig%3D%3D"，#  惠州
+    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBvcmc9IkNoaW5hbmV0IiAmJiBjaXR5PSJtYW9taW5nIg%3D%3D"，# 茂名
+    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBvcmc9IkNoaW5hbmV0IiAmJiBjaXR5PSJ6aHVoYWki",# 珠海
+    "https://fofa.info/result?qbase64=InVkcHh5IiAmJiBvcmc9IkNoaW5hbmV0IiAmJiBjaXR5PSJzaGFudG91Ig%3D%3D"  #汕头
+
 ]
 
 
